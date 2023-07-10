88bd465d8258108e3320ccdb5bb8ae01179e5356
jzemel93 <139079206+jzemel93@users.noreply.github.com>
2023-07-09 22:39:23 -0500

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,21 @@
+/*
+function validateForm() {
+    //browser already does some validation
+    //validate email
+    let fEmail = document.forms(["contact"]["fEmail"]);
+    //validate telephone #
+    let fTel = document.forms(["contact"]["fTel"]);
+
+    //if email & telephone input match patterns
+    success();
+    //else those input borders change to red if validation fails
+    invalidInput();
+}
+*/
+function success() {
+    //after form is successfully submitted, insert message as <br>(message) after submit button
+    
+}
+function invalidInput() {
+
+}
